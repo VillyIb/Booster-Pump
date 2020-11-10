5eb2803d528c7d7be73a746267e5a6db77daf722
VillyIb Jorgensen <vij@gtx.nu>
2020-11-10 22:01:16 +0100

    Adds Pressure Measurement Module AMS5812...

---
+++ BoosterPumpLibrary/Modules/AMS5812_0150_D_B_Module.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BoosterPumpLibrary.Commands;
+using BoosterPumpLibrary.Contracts;
+
+namespace BoosterPumpLibrary.Modules
+{
+    // ReSharper disable once UnusedMember.Global
+    public class AMS5812_0150_D_B_Module
+    {
+        private readonly ISerialConverter SerialPort;
+
+        public byte Address => 0x78;
+
+
+        public AMS5812_0150_D_B_Module(ISerialConverter serialPort)
+        {
+            SerialPort = serialPort;
+        }
+
+        public float Pressure { get; protected set; }
+
+        public float PressureCorrection { get; set; }
+
+        public float Temperature { get; protected set; }
+
+
+
+        public void ReadFromDevice()
+        {
+            var command = new ReadCommand{Address =  Address, LengthRequested =  4};
+            var response =  SerialPort.Execute(command);
+
+            // calculate Pressure and Temperature.
+        }
+    }
+}
---
+++ BoosterPumpTest/AMS5812_0150_D_B_ModuleShould.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BoosterPumpLibrary.Commands;
+using BoosterPumpLibrary.Contracts;
+using BoosterPumpLibrary.Modules;
+using NSubstitute;
+using NSubstitute.Routing.Handlers;
+using Xunit;
+
+namespace BoosterPumpTest
+{
+    public class AMS5812_0150_D_B_ModuleShould
+    {
+        private readonly AMS5812_0150_D_B_Module _Sut;
+        private readonly ISerialConverter _FakeSerialPort;
+
+        public AMS5812_0150_D_B_ModuleShould()
+        {
+            _FakeSerialPort = Substitute.For<ISerialConverter>();
+            _Sut = new AMS5812_0150_D_B_Module(_FakeSerialPort);
+        }
+
+        [Fact]
+        public void SendReadSequenceCallingReadFromDevice()
+        {
+            _Sut.ReadFromDevice();
+
+            //_FakeSerialPort.When()...ReturnAutoValue(0xAA, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00); // simulate measurement...
+
+            _FakeSerialPort.Received().Execute(Arg.Is<WriteCommand>(c => c.I2CDataAsHex == "78 00"));
+
+            Assert.Equal(55.5f, _Sut.Pressure); 
+            Assert.Equal(24.5f, _Sut.Temperature);
+        }
+
+    }
+}
