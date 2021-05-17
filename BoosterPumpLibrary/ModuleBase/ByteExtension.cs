2e93e435c6329bf71781fed760e656141afc00e7
Villy_Joergensen <villy.joergensen@wsa.com>
2021-05-17 20:43:43 +0200

    Renames file.

---
+++ BoosterPumpLibrary/ModuleBase/ByteExtension.cs
@@ -0,0 +1,35 @@
+﻿namespace BoosterPumpLibrary.ModuleBase
+{
+    /// <summary>
+    /// Enables operator overloading for + and = making an int assignable to a byte.
+    /// </summary>
+   public class ByteExtension
+    {
+        private readonly byte Payload;
+
+        public ByteExtension(int value)
+        {
+            Payload = (byte)value;
+        }
+
+        public static ByteExtension operator +(ByteExtension first, ByteExtension second)
+        {
+            return new ByteExtension(first.Payload + second.Payload);
+        }
+
+        public static ByteExtension operator +(ByteExtension first, byte second)
+        {
+            return new ByteExtension(first.Payload + second);
+        }
+
+        public static implicit operator byte(ByteExtension value)
+        {
+            return value.Payload;
+        }
+
+        public static implicit operator ByteExtension(int value)
+        {
+            return new ByteExtension(value);
+        }
+    }
+}
